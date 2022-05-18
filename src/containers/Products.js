4bfff8b741bb76f37b521461a13697d908d636b6
Dambadarjaa <57674891+Antaqor@users.noreply.github.com>
2022-05-18 18:05:20 +0800

    Product modify

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Product from "../components/Product";
 import "../styles/Product.css"
+import all from "../assets/all.svg";
 
 const Products = () => {
     return (
         <div className="container products">
+                <div className="col1">
+                    <h1 className="component-header">
+                        Ёндоотой бөөн бөөн бараа
+                        <span className="showall">Бүгдийг үзэх<img src={all} alt=""/></span>
+                    </h1>
+                </div>
             <div className="col5">
                 <Product/>
             </div>
